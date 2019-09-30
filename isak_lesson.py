fc7904466d9dabe1fa6a4411d18a70aa5f58373a
Pauline Högberg <pauline.hogberg@hyperisland.se>
2019-09-30 11:52:03 +0200

    Create isak_lesson.py
    Testing and a min infinitive loop.

---
+++ isak_lesson.py
@@ -0,0 +1,45 @@
+
+"""name = ""
+while name != "Isak":
+   print('Hey, I heard it is a super special day today! Who has a special day today?')
+   name = input()
+   if name == 'Isak':
+      print('OH, YAZ biss, Happy Birthday!')
+   else:
+      print('Try again dumbass')"""
+
+"""name = ""
+while name != "Astrid?":
+   print('have you met This big eyed and cray cray smart woman? She\'s the coolest and most gorge girl in Nacka!')
+   name = input()
+   if name == 'Astrid?':
+      print('Yeah! You met her! I wish I was more like her.')
+   else:
+      print('Naaah, not that bitch.. ' )"""
+
+# HOW TO MAKE LISTS AND HOW TO HANDLE THEM
+#How to print different parts in a definition of a argument.
+#remember that the first value ALWAYS is 0.
+"""fruit = ['apple', 'pear',3 ]
+print(fruit[0])"""
+
+#how to add vaulues into and argumnet.
+"""fruits = ['apple', 'pear']
+print(fruits)
+fruits.append('strawberry')
+fruits.append('blueberry')
+print(fruits)"""
+
+#How to change the value in the argument but don't have to find the original information.
+"""fruits = ['apple', 'pear', 'strawberry']
+fruits[1] = 'blueberry'
+print(fruits)"""
+
+#TUPOOL - coordinates, rectangles and cudders? or hold info, different typ of datatype.
+fruits = ['apple', 'pear', 'strawberry']
+position = (2, 3)
+color = (255, 255, 255)
+print(type(color))
+
+
+
